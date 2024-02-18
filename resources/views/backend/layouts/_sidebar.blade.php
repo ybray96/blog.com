fcbd747b9e5f161efd32bd23d999919a54e98eaf
Dastan <dastan.ibrai@gmail.com>
2024-02-18 22:12:09 +0600

    16 Active Class Sidebar Menu 2 Ways

--- resources/views/backend/layouts/_sidebar.blade.php
+++ resources/views/backend/layouts/_sidebar.blade.php
@@ -4,21 +4,21 @@
 <ul class="sidebar-nav" id="sidebar-nav">
 
   <li class="nav-item">
-    <a class="nav-link " href="{{ url('panel/dashboard')}}">
+    <a class="nav-link @if(Request::segment(2)!= 'dashboard') collapsed @endif " href="{{ url('panel/dashboard')}}">
       <i class="bi bi-grid"></i>
       <span>Dashboard</span>
     </a>
   </li>
 
   <li class="nav-item">
-    <a class="nav-link collapsed" href="{{ url('panel/user/list') }}">
+    <a class="nav-link @if(Request::segment(2)!= 'user') collapsed @endif " href="{{ url('panel/user/list') }}">
       <i class="bi bi-person"></i>
       <span>Users</span>
     </a>
   </li>
 
   <li class="nav-item">
-    <a class="nav-link collapsed" href="{{ url('panel/category/list') }}">
+    <a class="nav-link @if(Request::segment(2)!= 'category') collapsed @endif " href="{{ url('panel/category/list') }}">
       <i class="bi bi-person"></i>
       <span>Category</span>
     </a>
